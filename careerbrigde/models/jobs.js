0f0d666c561eb9cd75e8f2fa0fa06955ea9fc6a1
shamssali8937 <sa4857433@gmail.com>
2026-01-25 22:18:50 +0500

    created models for careerbrigde

---
+++ careerbrigde/models/jobs.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+const Schema=mongoose.Schema;
+
+const job=new Schema({
+     jobId:{
+        type:Number
+     },
+     provider:{
+        type:mongoose.Schema.ObjectId,
+         required:true,
+        ref:"Provider"
+     },
+      title:{
+        type:String,
+        required:true
+     },
+     location:{
+        type:String,
+        required:true
+     },
+     jobType:{
+        type:String,
+        required:true
+     },
+      salary:{
+        type:String,
+     },
+      description:{
+        type:String,
+        required:true
+     },
+     requirements:[String],
+     screeningQuestions:[String],
+     postDate:{
+        type:Date,
+        required:true,
+        default:Date.now()
+     },
+    lastDate:{
+        type:Date,
+        required:true
+     },
+     isdelete:{
+        type:Boolean,
+        default:false
+    }
+},{timestamps:true});
+
+export default mongoose.models.jobs || mongoose.model("jobs",jobs);
---
+++ careerbrigde/models/provider.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+const Schema=mongoose.Schema;
+
+const provider=new Schema({
+     providerId:{
+        type:Number
+     },
+     user:{
+        type:mongoose.Schema.ObjectId,
+         required:true,
+        ref:"Users"
+     },
+     contact:{
+        type:String,
+        required:true
+     },
+      companyName:{
+        type:String,
+        required:true
+     },
+     goalOfCompany:{
+        type:String,
+        required:true
+     },
+     address:{
+        type:String,
+        required:true
+     },
+     positionInCompany:{
+        type:String,
+        required:true
+     },
+      tenureInTimePeriod:{
+        type:String,
+        required:true
+     },
+     aboutCompany:{
+        type:String,
+        required:true
+     },
+     isdelete:{
+        type:Boolean,
+        default:false
+    }
+     
+},{timestamps:true});
+
+
+export default mongoose.models.provider || mongoose.model("provider",provider);
