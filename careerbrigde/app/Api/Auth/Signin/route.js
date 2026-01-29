d641542078d34fb7d581e2588899bfdba433c63b
shamssali8937 <sa4857433@gmail.com>
2026-01-29 11:17:41 +0500

    set the structure

--- careerbrigde/app/Api/Auth/Signin/route.js
+++ careerbrigde/app/Api/Auth/Signin/route.js
@@ -1,21 +1,5 @@
 import { signinUser } from "@/controllers/userController";
-import { jwtAcessTokenCreator, jwtRefreshTokenCreator } from "@/Lib/Auth/JwtCreator";
-import mongoose from "mongoose";
-import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req) {
-
-   try{
-    const data=await req.json();
-    const user= await signinUser(data);
-    const accessToken=jwtAcessTokenCreator(user);
-    const refreshToken=jwtRefreshTokenCreator(user);
-    if(user){
-        return NextResponse.json({ message: "Succssfully signedIn",User:user,AccessToken:accessToken,RefreshToken:refreshToken},
-          { status: 200 });
-    }
-   }catch(err){
-       return NextResponse.json({err:err.message},{status:400});
-   }
-    
+      return await signinUser(req)
 }
--- careerbrigde/controllers/userController.js
+++ careerbrigde/controllers/userController.js
@@ -1,6 +1,6 @@
 import { createUser, singin } from "@/services/userServices";
 import { NextResponse } from "next/server";
-
+import { jwtAcessTokenCreator, jwtRefreshTokenCreator } from "@/Lib/Auth/JwtCreator";
 export const signupUser=async(req)=>{
        try{
             const newUser=await createUser(req);
@@ -11,23 +11,35 @@ export const signupUser=async(req)=>{
               return NextResponse.json({ message: "User created successfully" },{ status: 200 });    
 
        }catch(err){
-           console.log(err);
+           //console.log(err);
           return NextResponse.json({ message: "User creation failed" },{ status: 500 });
        }
 }
 
 
-export const  signinUser=async(body)=>{
-  const {email,password}=body;
-  const data={
-    email:email,
-    password:password
-  }
-  if(!email||!password){
-    throw new Error("Required credentials are missing");
-  }
+export const  signinUser=async(req)=>{
 
-  return await singin(data);
+  try{
+    const reqBody=await req.json();
+    const {email,password}=reqBody;
+    const data={
+      email:email,
+      password:password
+    }
+    if(!email||!password){
+      throw new Error("Required credentials are missing");
+    }
+
+    const user= await singin(data);
+    if(user){
+        const accessToken=jwtAcessTokenCreator(user);
+        const refreshToken=jwtRefreshTokenCreator(user);
+        return NextResponse.json({ message: "Succssfully signedIn",User:user,AccessToken:accessToken,RefreshToken:refreshToken},
+          { status: 200 });
+    }
+   }catch(err){
+       return NextResponse.json({err:err.message},{status:400});
+   }
 
 }
 
